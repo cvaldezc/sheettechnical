a7ec9c5ac3a4496cf3aaffcd8f603d5bff81107c
Christian Valdez Chavez <cvaldezchavez@gmail.com>
2017-07-24 16:52:43 -0500

    change config in files

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-    entry: "./src/index.ts",
+    entry: [
+        path.join(__dirname, './src/js/signin.ts')
+    ],
     output: {
         path: path.join(__dirname, './public/js'),
-        filename: "[name].bundle.js"
+        filename: '[name].bundle.js'
     },
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: 'source-map',
     resolve: {
         // Add '.ts' as resolvable extensions.
-        extensions: [".ts", ".js"]
+        extensions: ['.ts', '.js']
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                use: ["source-map-loader"],
-                enforce: "pre"
+                use: ['source-map-loader'],
+                enforce: 'pre'
             },
             {
                 test: /\.ts$/,
-                use: ["ts-loader"]
+                use: ['ts-loader']
             }
         ]
     }
-    // Omit "externals" if you don't have any. Just an example because it's
+    // Omit 'externals' if you don't have any. Just an example because it's
     // common to have them.
 };
